7000c6fa94e117ce468bcb9c189750ac59798614
nithish-kunjimon <nithishak22@gmail.com>
2020-07-05 00:31:39 +0530

    Created form,POST route for adding new campground

--- V1/app.js
+++ V1/app.js
@@ -1,32 +1,53 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const axios = require("axios");
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
-app.set("view engine", "ejs");
+let campgrounds = [{
+    name: "Salmon Creek",
+    image: "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350",
+  },
+  {
+    name: "James Cameron",
+    image: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350",
+  },
+  {
+    name: "Alex Gardner",
+    image: "https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e507440762778d19f4acc_340.jpg",
+  },
+];
 
-app.get("/", (req, res) => {
+app.use(bodyParser.urlencoded({
+  encoded: true
+}));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
   res.render("landing");
 });
 
-app.get("/campgrounds", (req, res) => {
-  let campgrounds = [{
-      name: "Salmon Creek",
-      image: "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350",
-    },
-    {
-      name: "James Cameron",
-      image: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=350",
-    },
-    {
-      name: "Alex Gardner",
-      image: "https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e507440762778d19f4acc_340.jpg",
-    },
-  ];
-  res.render("campgrounds", {
+app.get('/campgrounds', (req, res) => {
+  res.render('campgrounds', {
     campgrounds: campgrounds
   });
 });
 
-app.listen(3000, function () {
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/campgrounds', (req, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = {
+    name: name,
+    image: image
+  };
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds');
+});
+
+
+app.listen(3000, () => {
   console.log("Server Has Started!");
 });
